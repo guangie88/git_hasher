7904da42a3c097c46c2d19e6ef31dd5276205bdc
Chen Weiguang <chen.weiguang@gmail.com>
2017-08-12 12:31:31 +0800

    Fixed unable to create the new commit. Changed to using clap to interpret the program arguments since it displays more user friendly help and error messages.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+extern crate clap;
 extern crate crypto_hash;
 #[macro_use] extern crate derive_new;
-extern crate docopt;
 #[macro_use] extern crate error_chain;
 extern crate file;
 extern crate git2;
@@ -15,9 +15,9 @@ extern crate strum;
 #[macro_use] extern crate strum_macros;
 extern crate toml;
 
+use clap::{App, AppSettings, Arg, SubCommand};
 use crypto_hash::Algorithm;
-use docopt::Docopt;
-use git2::{IndexAddOption, ObjectType, Repository, ResetType, Signature, StatusOptions};
+use git2::{Commit, ErrorCode, IndexAddOption, ObjectType, Reference, Repository, ResetType, Signature, StatusOptions};
 use git2::build::CheckoutBuilder;
 use regex::Regex;
 use std::collections::{BTreeMap, BTreeSet};
@@ -37,27 +37,28 @@ pub mod errors {
 
 use errors::ResultExt;
 
-// parsing of arguments
-const USAGE: &'static str = "
-Git Hasher program to generate hash file within git working directory.
-
-Usage:
-    git_hasher [--conf=<conf>] [--log=<log>]
-    git_hasher addcommit <commit-msg> [--conf=<conf>] [--log=<log>]
-    git_hasher (-h | --help)
-
-Options:
-    -h --help       Show this help message.
-    --conf=<conf>   Config file path [default: git_hasher_config.toml].
-    --log=<log>     Log config file path.
-";
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct ArgConfig {
-    flag_conf: String,
-    flag_log: Option<String>,
-    cmd_addcommit: bool,
-    arg_commit_msg: Option<String>,
+    conf: String,
+    log: Option<String>,
+    ops: ArgOps,
+}
+
+#[derive(Debug)]
+enum ArgOps {
+    Hash,
+    AddCommit {
+        msg: String,
+    },
+}
+
+struct HashConfig {
+    file_config: FileConfig,
+}
+
+struct AddCommitConfig {
+    file_config: FileConfig,
+    msg: String,
 }
 
 #[derive(Deserialize)]
@@ -115,10 +116,10 @@ enum NdType {
     New, Deleted,
 }
 
-fn init_logger(flag_log: &Option<String>) -> errors::Result<()> {
-    if let &Some(ref flag_log) = flag_log {
-        log4rs::init_file(flag_log, Default::default())
-            .chain_err(|| format!("Unable to initialize log4rs logger with the given config file at '{}'", flag_log))?;
+fn init_logger(log: &Option<String>) -> errors::Result<()> {
+    if let &Some(ref log) = log {
+        log4rs::init_file(log, Default::default())
+            .chain_err(|| format!("Unable to initialize log4rs logger with the given config file at '{}'", log))?;
     } else {
         simple_logger::init()
             .chain_err(|| "Unable to initialize default logger")?;
@@ -179,32 +180,19 @@ where I: Iterator<Item=(&'a str, &'a str)> {
     count
 }
 
-fn run() -> errors::Result<()> {
-    // initialization
-    let arg_config: ArgConfig = Docopt::new(USAGE)
-        .and_then(|d| d.deserialize())
-        .unwrap_or_else(|e| e.exit());
+fn hash_impl(hash_config: HashConfig) -> errors::Result<()> {
+    let (algo_enum, algo) = get_algo(&hash_config.file_config.hash_meth)?;
 
-    // let arg_config = ArgConfig::from_args();
-    init_logger(&arg_config.flag_log)?;
-
-    let config_str = read_string_from_file(&arg_config.flag_conf)?;
-
-    let config: FileConfig = toml::from_str(&config_str)
-        .chain_err(|| format!("Unable to parse config as required toml format: {}", config_str))?;
-
-    let (algo_enum, algo) = get_algo(&config.hash_meth)?;
-
-    let regexes = config.regex_matches.iter()
+    let regexes = hash_config.file_config.regex_matches.iter()
         .map(|s| Regex::new(s).chain_err(|| format!("Unable to convert string: {} to regex pattern", s)))
         .collect::<Result<Vec<Regex>, errors::Error>>()?;
 
     // git section
-    let repo = Repository::open(&config.git_path)
-        .chain_err(|| format!("Unable to find local git working directory at '{}'", config.git_path))?;
+    let repo = Repository::open(&hash_config.file_config.git_path)
+        .chain_err(|| format!("Unable to find local git working directory at '{}'", hash_config.file_config.git_path))?;
 
     // attempt to parse the current commit (destructive) hash yaml first
-    let prev_hash_collection = match repo.status_file(&Path::new(&config.hash_path)) {
+    let prev_hash_collection = match repo.status_file(&Path::new(&hash_config.file_config.hash_path)) {
         Ok(status) => {
             debug!("Hash path status: {:?}", status);
 
@@ -215,22 +203,23 @@ fn run() -> errors::Result<()> {
                 let (tx, rx) = mpsc::channel();
 
                 opts.force()
-                    .path(&config.hash_path)
+                    .path(&hash_config.file_config.hash_path)
                     .update_index(false)
                     .progress(move |_: Option<&Path>, current_count: usize, total_count: usize| {
                         if current_count == total_count {
                             if let Err(e) = tx.send(Some(())) {
-                                error!("Error sending Some value into git checkout channel: {}. Process will hang, press CTRL-C to terminate...", e);
+                                error!("Error sending Some value into git checkout channel: {}. \
+                                    Process will hang, press CTRL-C to terminate...", e);
                             }
                         }
                     });
 
                 repo.checkout_index(None, Some(&mut opts))
-                    .chain_err(|| format!("Unable to perform checkout on head for hash yaml at '{}'", config.hash_path))?;
+                    .chain_err(|| format!("Unable to perform checkout on head for hash yaml at '{}'", hash_config.file_config.hash_path))?;
 
                 rx.recv().chain_err(|| "Unable to receive value from git checkout channel")?;
 
-                let prev_content = match read_string_from_file(&config.hash_path) {
+                let prev_content = match read_string_from_file(&hash_config.file_config.hash_path) {
                     Ok(content) => Some(content),
                     Err(e) => {
                         error!("Assuming no previous hash, unable to read content from hash yaml file: {}", e);
@@ -253,7 +242,7 @@ fn run() -> errors::Result<()> {
                 // highly possibly the status is STATUS_WT_NEW
                 // and assume that it is
                 warn!("Found new hash yaml file in working tree at '{}', continuing as if the next created hash yaml file is NEW",
-                    config.hash_path);
+                    hash_config.file_config.hash_path);
 
                 None
             }
@@ -261,7 +250,7 @@ fn run() -> errors::Result<()> {
 
         Err(e) => {
             warn!("{}. No previous hash yaml file found at '{}', continuing as if the next created hash yaml file is NEW",
-                e, config.hash_path);
+                e, hash_config.file_config.hash_path);
 
             None
         },
@@ -276,12 +265,12 @@ fn run() -> errors::Result<()> {
             .recurse_untracked_dirs(true);
 
         repo.statuses(Some(&mut options))
-            .chain_err(|| format!("Unable to obtain statuses in the local git working directory at '{}'", config.git_path))?
+            .chain_err(|| format!("Unable to obtain statuses in the local git working directory at '{}'", hash_config.file_config.git_path))?
     };
 
     let filtered_paths: Vec<String> = statuses.iter()
         .filter_map(|s| s.path().map(|p| p.to_owned()))
-        .filter(|p| p != &config.hash_path && regexes.iter().any(|r| r.is_match(p)))
+        .filter(|p| p != &hash_config.file_config.hash_path && regexes.iter().any(|r| r.is_match(p)))
         .collect();
     
     let hash_elems: BTreeSet<HashElement> = filtered_paths.into_iter()
@@ -301,19 +290,19 @@ fn run() -> errors::Result<()> {
     // serialize into file section
     let hash_collection = HashCollection::new(algo_enum, hash_elems);
 
-    let mut hash_file = File::create(&config.hash_path)
-        .chain_err(|| format!("Unable to create hash file at '{}'", config.hash_path))?;
+    let mut hash_file = File::create(&hash_config.file_config.hash_path)
+        .chain_err(|| format!("Unable to create hash file at '{}'", hash_config.file_config.hash_path))?;
 
     let hash_collection_str = serde_yaml::to_string(&hash_collection)
         .chain_err(|| "Unable to serialize hash collection into TOML")?;
 
     hash_file.write_all(hash_collection_str.as_bytes())
-        .chain_err(|| format!("Unable to write into hash file at '{}'", config.hash_path))?;
+        .chain_err(|| format!("Unable to write into hash file at '{}'", hash_config.file_config.hash_path))?;
 
     // compare both the hash collections
     let curr_elems = &hash_collection.elements;
     
-    let has_changes = if let Some(prev_hash_collection) = prev_hash_collection {
+    if let Some(prev_hash_collection) = prev_hash_collection {
         if &prev_hash_collection.meth == &hash_collection.meth {
             let prev_elems = &prev_hash_collection.elements;
 
@@ -361,9 +350,6 @@ fn run() -> errors::Result<()> {
             info!(" Deleted count: {}", deleted_entries_count);
             info!("Modified count: {}", modified_entries_count);
             info!("   Total count: {}", curr_elems.len());
-
-            // indicate if the index is different from working tree
-            new_entries_count > 0 || deleted_entries_count > 0 || modified_entries_count > 0
         } else {
             // not allowed to proceed
             bail!("Previous hash collection uses method: {}, while current hash collection uses method: {}, which is not comparable",
@@ -372,77 +358,195 @@ fn run() -> errors::Result<()> {
     } else {
         info!("All files in current hash yaml file are NEW");
         info!("Total count: {}", curr_elems.len());
-
-        // indicate if the index is different from working tree
-        curr_elems.len() > 0
     };
-
-    // performs possibly additional operations
-    if let &Some(ref arg_commit_msg) = &arg_config.arg_commit_msg {
-        if arg_commit_msg.is_empty() {
-            bail!("Commit message cannot be empty for addcommit operation");
-        }
-
-        if !has_changes {
-            bail!("No changes found in working tree for addcommit operation");
-        }
-
-        let username = config.username.as_ref()
-            .ok_or_else(|| format!("No username in '{}' provided for addcommit option", arg_config.flag_conf))?;
-
-        let email = config.email.as_ref()
-            .ok_or_else(|| format!("No email address in '{}' provided for addcommit option", arg_config.flag_conf))?;
-
-        let mut index = repo.index()
-            .chain_err(|| "Unable to get index of git local repository")?;
-
-        index.add_all(["."].iter(), IndexAddOption::empty(), None)
-            .chain_err(|| "Unable to add all files into index for git local repository")?;
-
-        let new_tree_oid = index.write_tree()
-            .chain_err(|| "Unable to write tree using the updated index")?;
-
-        let new_tree = repo.find_tree(new_tree_oid)
-            .chain_err(|| "Unable to get tree from new OID")?;
-
-        let head_commit = repo.head()
-            .chain_err(|| "Unable to get the reference of HEAD")?
-            .resolve()
-            .chain_err(|| "Unable to resolve the reference of HEAD")?
-            .peel(ObjectType::Commit)
-            .chain_err(|| "Unable to peel after resolving reference of HEAD")?
-            .into_commit()
-            .map_err(|_| git2::Error::from_str("Cannot find commit"))
-            .chain_err(|| "Unable to convert into commit after peeling")?;
-
-        let author = Signature::now(username, email)
-            .chain_err(|| "Unable to create signature")?;
-
-        let committer = author.clone();
-        
-        let new_commit_oid = repo.commit(
-            Some("HEAD"),
-            &author,
-            &committer,
-            arg_commit_msg,
-            &new_tree,
-            &[&head_commit])
-            .chain_err(|| "Unable to perform commit into local git repository")?;
-
-        let new_commit = repo.find_commit(new_commit_oid)
-            .chain_err(|| "Unable to find new commit from OID")?;
-
-        repo.reset(new_commit.as_object(), ResetType::Hard, None)
-            .chain_err(|| "Unable to reset HEAD to the new commit")?;
-
-        info!("Successfully added and committed new changes!");
-    }
 
     Ok(())
 }
 
+fn addcommit_impl(addcommit_config: AddCommitConfig) -> errors::Result<()> {
+    // performs possibly additional operations
+    if addcommit_config.msg.is_empty() {
+        bail!("Commit message cannot be empty for addcommit operation");
+    }
+
+    let repo = Repository::open(&addcommit_config.file_config.git_path)
+        .chain_err(|| format!("Unable to find local git working directory at '{}'", addcommit_config.file_config.git_path))?;
+
+    // default way of checking repo status
+    let mut status_options = StatusOptions::default();
+    status_options.include_untracked(true);
+
+    let no_change = repo.statuses(Some(&mut status_options))
+        .chain_err(|| format!("Unable to obtain statuses in the local git working directory at '{}'", addcommit_config.file_config.git_path))?
+        .is_empty();
+
+    if no_change {
+        bail!("No changes found in working tree for addcommit operation");
+    }
+
+    let username = addcommit_config.file_config.username.as_ref()
+        .ok_or_else(|| "No username in file config provided for addcommit option")?;
+
+    let email = addcommit_config.file_config.email.as_ref()
+        .ok_or_else(|| "No email address in file config provided for addcommit option")?;
+
+    let mut index = repo.index()
+        .chain_err(|| "Unable to get index of git local repository")?;
+
+    index.add_all(["."].iter(), IndexAddOption::empty(), None)
+        .chain_err(|| "Unable to add all files into index for git local repository")?;
+
+    let new_tree_oid = index.write_tree()
+        .chain_err(|| "Unable to write tree using the updated index")?;
+
+    let new_tree = repo.find_tree(new_tree_oid)
+        .chain_err(|| "Unable to get tree from new OID")?;
+
+    enum HeadRef<'repo> {
+        Ref(Reference<'repo>),
+        UnbornBranch,
+    }
+
+    enum HeadCommit<'repo> {
+        Commit(Commit<'repo>),
+        UnbornBranch,
+    }
+
+    // may not have any reference if this is the very first commit
+    let head_ref = repo.head()
+        .map(|r| HeadRef::Ref(r))
+        .or_else(|err| {
+            if err.code() == ErrorCode::UnbornBranch {
+                Ok(HeadRef::UnbornBranch)
+            } else {
+                Err(err)
+            }
+        })
+        .chain_err(|| "Unable to get the reference of HEAD")?;
+
+    let head_commit = match head_ref {
+        HeadRef::Ref(r) => {
+            let commit = r.resolve().chain_err(|| "Unable to resolve the reference of HEAD")?
+                .peel(ObjectType::Commit)
+                .chain_err(|| "Unable to peel after resolving reference of HEAD")?
+                .into_commit()
+                .map_err(|_| git2::Error::from_str("Cannot find commit"))
+                .chain_err(|| "Unable to convert into commit after peeling")?;
+
+            HeadCommit::Commit(commit)
+        },
+
+        HeadRef::UnbornBranch => HeadCommit::UnbornBranch,
+    };
+
+    let author = Signature::now(username, email)
+        .chain_err(|| "Unable to create signature")?;
+
+    let committer = author.clone();
+    let update_ref = Some("HEAD");
+
+    let new_commit_oid = match head_commit {
+        HeadCommit::Commit(commit) => repo.commit(
+            update_ref, &author, &committer, &addcommit_config.msg, &new_tree, &[&commit]),
+
+        HeadCommit::UnbornBranch => repo.commit(
+            update_ref, &author, &committer, &addcommit_config.msg, &new_tree, &[]),
+
+    }.chain_err(|| "Unable to perform commit into local git repository")?;
+
+    let new_commit = repo.find_commit(new_commit_oid)
+        .chain_err(|| "Unable to find new commit from OID")?;
+
+    repo.reset(new_commit.as_object(), ResetType::Hard, None)
+        .chain_err(|| "Unable to reset HEAD to the new commit")?;
+
+    info!("Successfully added and committed new changes!");
+
+    Ok(())
+}
+
+fn run(arg_config: ArgConfig) -> errors::Result<()> {
+    init_logger(&arg_config.log)?;
+
+    let config_str = read_string_from_file(&arg_config.conf)?;
+
+    let config: FileConfig = toml::from_str(&config_str)
+        .chain_err(|| format!("Unable to parse config as required toml format: {}", config_str))?;
+
+    match &arg_config.ops {
+        &ArgOps::Hash => {
+            hash_impl(HashConfig {
+                file_config: config,
+            })
+        },
+
+        &ArgOps::AddCommit { ref msg } => {
+            addcommit_impl(AddCommitConfig {
+                file_config: config,
+                msg: msg.to_owned(),
+            })
+        },
+    }
+}
+
 fn main() {
-    match run() {
+    const HASH_SUB_CMD: &'static str = "hash";
+    const ADDCOMMIT_SUB_CMD: &'static str = "addcommit";
+    const VERSION_STR: &'static str = "0.1.0";
+    const AUTHOR_STR: &'static str = "Chen Weiguang <chen.weiguang@gmail.com>";
+
+    // argument parsing
+    let matches = App::new("Git Hasher")
+        .version(VERSION_STR)
+        .author(AUTHOR_STR)
+        .about("Generate hash file within git working directory.")
+        .arg(Arg::with_name("conf")
+            .short("c")
+            .long("conf")
+            .help("Config file path"))
+        .arg(Arg::with_name("log")
+            .short("l")
+            .long("log")
+            .help("Log config file path"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name(HASH_SUB_CMD)
+            .about("Performs hashing on files within git working directory filtered by .gitignore.")
+            .version(VERSION_STR)
+            .author(AUTHOR_STR))
+        .subcommand(SubCommand::with_name("addcommit")
+            .about("Performs git add . --all & git commit with a required commit message.")
+            .version(VERSION_STR)
+            .author(AUTHOR_STR)
+            .arg(Arg::with_name("msg")
+                .help("Message to commit")
+                .required(true)
+                .index(1)))
+        .get_matches();
+
+    let conf = matches.value_of("conf").unwrap_or("git_hasher_config.toml").to_owned();
+    let log = matches.value_of("log").map(|log| log.to_owned());
+
+    let ops = if let Some(_) = matches.subcommand_matches(HASH_SUB_CMD) {
+        Some(ArgOps::Hash)
+    } else if let Some(submatches) = matches.subcommand_matches(ADDCOMMIT_SUB_CMD) {
+        let msg = submatches.value_of("msg")
+            .expect("msg argument must be set from the command line")
+            .to_owned();
+
+        Some(ArgOps::AddCommit{ msg: msg })
+    } else {
+        None
+    };
+
+    let ops = ops.expect("ArgOps cannot be None by the structure of argument parsing");
+
+    let arg_config = ArgConfig {
+        conf: conf,
+        log: log,
+        ops: ops,
+    };
+
+    match run(arg_config) {
         Ok(_) => {
             info!("Program completed!");
             process::exit(0)
